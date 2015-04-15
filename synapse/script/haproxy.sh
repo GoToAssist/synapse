a9ca6968d847f65070a588145afdfd358efbd0df
Christian Bak <christian.bak@citrix.com>
2015-04-15 15:13:03 +0200

    Fix haproxy start script

--- synapse/script/haproxy.sh
+++ synapse/script/haproxy.sh
@@ -3,7 +3,7 @@
 ${HAPROXY_PID_FILE:=/opt/boxen/data/haproxy/haproxy.pid}
 ${HAPROXY_CFG_FILE:=/opt/boxen/config/haproxy/haproxy.cfg}
 
-if [ -f $PID_FILE ]; then
+if [ -f $HAPROXY_PID_FILE ]; then
     haproxy -f $HAPROXY_CFG_FILE -p $HAPROXY_PID_FILE -sf $(cat $HAPROXY_PID_FILE)
 else
     haproxy -f $HAPROXY_CFG_FILE -p $HAPROXY_PID_FILE
